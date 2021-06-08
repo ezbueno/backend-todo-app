7d9987babfeb5a79b5248857ae6eb9d7f223c68b
ezbueno <ezandrobueno@yahoo.com.br>
2021-06-08 17:16:45 -0300

    Criado a interface TodoMapper para mapeamento de DTOs

---
+++ src/main/java/bueno/ezandro/todo/mapper/TodoMapper.java
@@ -0,0 +1,22 @@
+package bueno.ezandro.todo.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import bueno.ezandro.todo.model.Todo;
+import bueno.ezandro.todo.model.dto.TodoDTO;
+
+@Mapper
+public interface TodoMapper {
+
+	TodoMapper INSTANCE = Mappers.getMapper(TodoMapper.class);
+
+	Todo toModel(TodoDTO todoDTO);
+
+	TodoDTO toDTO(Todo todo);
+
+	List<TodoDTO> toDTO(List<Todo> todos);
+
+}
