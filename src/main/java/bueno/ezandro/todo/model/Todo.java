86a5e24303ec4ceb9f8df5d5956c2a6c9008faf1
ezbueno <ezandrobueno@yahoo.com.br>
2021-06-08 15:25:29 -0300

    Criado a entidade Todo

---
+++ src/main/java/bueno/ezandro/todo/model/Todo.java
@@ -0,0 +1,38 @@
+package bueno.ezandro.todo.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Todo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "done")
+	private Boolean done;
+	
+	@Column(name = "createdDate")
+	private LocalDateTime createdDate;
+	
+	@Column(name = "doneDate")
+	private LocalDateTime doneDate;
+	
+}
